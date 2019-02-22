2dc558f9f476dfd69ea0cc75dc2804fe911a7976
cavonte <edouard.12@live.ca>
2019-02-21 20:48:00 -0500

    added form

--- app/UIManager.jsx
+++ app/UIManager.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import data from "./data"
 import Header from "./header";
 import List from "./List";
+import Item from "./Item";
+import ItemForm from "./ItemForm";
+
 
 console.log(data);
 
@@ -13,26 +16,55 @@ class UIManager extends React.Component
         super();
         this.state = {
             listName : "The List",
-            list : data.music
+            list : data.music,
+            searchTerm: '',
+            formFields: 
+            {
+                id:'',
+                title:'',
+                artist:'',
+                album:'',
+            },
         }
     }
 
     searchList(event) 
 	{
-		var search_term = event.target.value;
-		console.log(search_term);
+        var search_term = event.target.value;
+        console.log('${this.state.search_term} -> ${searchTerm}');
+        
+        this.setState({
+        searchTerm : search_term
+        });
 	}
 
     render()
     {
+        var list = this.state.list;
+        var search_term = this.state.searchTerm;
+        var filteredList;
+
+        if(!search_term)
+        {
+            filteredList = list;
+        } else 
+        {
+            filteredList = list.filter(function(item){
+                return item.title.toLowerCase().includes(search_term.toLowerCase());
+            });            
+        }
+
         return <div>
             <Header/>
             <div className="options">
                 <input type="text"
                 placeholder="Filter"
-                onChange={this.searchList}/>
+                onChange={(event) => this.searchList(event)}/>
             </div>
-            <List list={this.state.list} name={this.state.listName}></List>
+
+
+            <List list={filteredList} name={this.state.listName}></List>
+            <ItemForm  item={this.state.formFields}/>
         </div>
     }
 }
--- app/index.jsx
+++ app/index.jsx
@@ -6,8 +6,7 @@ class App extends React.Component {
 	
 	render() {
 		return <div>
-				<UIManager></UIManager>
-
+				<UIManager/>
 				</div>;
 	}
 }
