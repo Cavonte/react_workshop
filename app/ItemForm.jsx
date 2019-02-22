cc3bcbb97036d897b01503a188044c7b565514e5
cavonte <edouard.12@live.ca>
2019-02-22 02:31:34 -0500

    completed chapter 2

--- app/ItemForm.jsx
+++ app/ItemForm.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
 class ItemForm extends React.Component {
+
+    hideForm()
+    {
+        console.log("Form Close");
+        let modal = document.querySelector('.modal');
+        modal.style.display = "none";
+    }
+
+    submitForm(event)
+    {
+        event.preventDefault();
+        //Prevents the page from reloading on the form submission
+        console.log("Form Sbumitted");
+        this.props.createItem();
+    }
+
     render() {
 
         var item = this.props.item;
@@ -10,35 +26,40 @@ class ItemForm extends React.Component {
         }
 
         return (
-            <div>
+            <div className="modal">
                 <form>
-                    <div className="close_form">
-                        <span>[🗙]</span>
+                    <div className="close_form">                        
+                        <span onClick={this.hideForm} >[🗙]</span>
                     </div>
                     <h3>Create a new item</h3>
                     <p>
                         <label>ID:</label>
                         <input name="id" 
-                               value={item.id} />
+                               value={item.id}
+                               onChange={(event) => this.props.onChangeFormInput(event)} 
+                               />
                     </p>
                     <p>
                         <label>Title:</label>
                         <input name="title" 
-                               value={item.title} />
+                               value={item.title} 
+                               onChange={(event) => this.props.onChangeFormInput(event)} />
                     </p>
                     <p>
                         <label>Artist:</label>
                         <input name="artist" 
-                               value={item.artist} />
+                               value={item.artist}
+                               onChange={(event) => this.props.onChangeFormInput(event)} />
                     </p>
                     <p>
                         <label>Album:</label>
                         <input name="album" 
-                               value={item.album} />
+                               value={item.album} 
+                                onChange={(event) => this.props.onChangeFormInput(event)} />
                     </p>
 
                     <div className="create">
-                        <button>
+                        <button onClick={(event) => this.submitForm(event)}>
                             CREATE
                         </button>
                     </div>
--- app/UIManager.jsx
+++ app/UIManager.jsx
@@ -28,6 +28,57 @@ class UIManager extends React.Component
         }
     }
 
+    additionComplete()
+    {
+        let modal = document.querySelector('.modal');
+        modal.style.display = "none";
+    }
+
+    createItem()
+    {
+        //store the current field
+        var item = this.state.formFields;
+        //store current list innto an iterm
+        var currentListItems = [...this.state.list];
+        //push new item in list
+        currentListItems.push(item);
+        //apply changes with setState
+        //update the list
+        //reset the field
+        this.setState({
+            list: currentListItems,
+            formFields: 
+            {
+                id:'',
+                title:'',
+                artist:'',
+                album:'',
+            },
+        })
+        this.additionComplete();
+
+    }
+
+    showForm()
+    {
+        let modal = document.querySelector('.modal');
+        modal.style.display = "block";
+    }
+
+    onChangeFormInput(event)
+    {
+        // console.log("Input changed");
+        // console.log(event.target.name);
+        // console.log(event.target.value);
+        var currentListFields = {...this.state.formFields};
+        currentListFields[event.target.name] = event.target.value;
+        this.setState({
+            formFields: currentListFields
+        });
+        console.log(this.state.formFields);
+
+    }
+
     searchList(event) 
 	{
         var search_term = event.target.value;
@@ -60,11 +111,15 @@ class UIManager extends React.Component
                 <input type="text"
                 placeholder="Filter"
                 onChange={(event) => this.searchList(event)}/>
+                <span className="add" onClick={this.showForm}> [+] </span>
             </div>
 
 
             <List list={filteredList} name={this.state.listName}></List>
-            <ItemForm  item={this.state.formFields}/>
+            <ItemForm  item={this.state.formFields}
+               onChangeFormInput={(event) => this.onChangeFormInput(event)}
+                createItem={() => this.createItem(event)}
+                />
         </div>
     }
 }
